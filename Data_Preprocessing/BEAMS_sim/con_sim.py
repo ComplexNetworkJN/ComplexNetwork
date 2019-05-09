aa5138ad448e5e4c9c30bee45b74de33717e3e3d
ComplexNetworkJN <50008299+ComplexNetworkJN@users.noreply.github.com>
2019-05-09 13:27:55 +0800

    Update con_sim.py

--- Data_Preprocessing/BEAMS_sim/con_sim.py
+++ Data_Preprocessing/BEAMS_sim/con_sim.py
@@ -6,18 +6,16 @@ Created on Fri Mar 22 15:12:04 2019
 """
 
 def con_sim(name1,num1,name2,num2):
-    print(name1,name2,num1,num2)
     file = open(f'{name}.evals','w')
-    #print(name1,num1)
     for i in range(0,int(num1)):
-        #print(i)
+    
         for j in range(0,int(num2)):
-            print("%s%d\t%s%d\t1.0\n" %(name1,i,name2,j))
+            #print("%s%d\t%s%d\t1.0\n" %(name1,i,name2,j))
             file.write("%s%d\t%s%d\t1.0\n" %(name1,i,name2,j))
     file.close()
 
-name=input("Input species' name:")
-num1,num2 = input("Input number of nodes:").split(',')
+name=input("Input species' name:\n")
+num1,num2 = input("Input number of nodes:\n").split(',')
 
 lineARR = name.split('-')
 name1 = lineARR[0]
