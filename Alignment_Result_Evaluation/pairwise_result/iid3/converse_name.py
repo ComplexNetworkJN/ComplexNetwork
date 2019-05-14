09674290e82bd261c9c71bf86a81528709e83f73
wws <351483565@qq.com>
2019-05-13 17:03:33 -0700

    added iid3 pairwise alignment result scripts

---
+++ Alignment_Result_Evaluation/pairwise_result/iid3/converse_name.py
@@ -0,0 +1,56 @@
+import pickle
+
+s=set()
+l=["human","mouse","rat"]
+for x in l:
+    for y in l:
+        if x!=y:
+            s.add("-".join(sorted([x,y])))
+L=[each for each in s]
+
+
+fmouse=open(f"mouse_dict_reverse","rb")
+frat=open(f"rat_dict_reverse","rb")
+fhuman=open(f"human_dict_reverse","rb")
+
+_human=pickle.load(fhuman)
+_mouse=pickle.load(fmouse)
+_rat=pickle.load(frat)
+
+
+fmouse.close()
+frat.close()
+fhuman.close()
+
+
+for each in sorted(L):
+    sp1,sp2=each.split("-")
+    fread =open(each+".txt"  ,"r")
+    fwrite=open(each+".align","w")
+    SAVED=set()
+    for line in fread:
+        
+        p1,p2=line.rstrip().split(" ")
+        if "ra" in p1:
+            p1="rt" + p1[2:]
+        if "ra" in p2:
+            p2="rt" + p2[2:]
+    #rat are renamed rt123, rt124. So we need to first change ra to rt;
+    #ps: if in the original input data, rat has already been changed to rt123...
+    #then, just comment out the above if statements;
+           
+        if (eval('_'+sp1)[p1],eval('_'+sp2)[p2]) not in SAVED:
+            SAVED.add((eval('_'+sp1)[p1],eval('_'+sp2)[p2]))
+            fwrite.write(f"{eval('_'+sp1)[p1]} {eval('_'+sp2)[p2]}\n")
+        else:
+            print(p1,p2,sp1,sp2)
+            print(eval('_'+sp1)[p1],eval('_'+sp2)[p2])
+            print((eval('_'+sp1)[p1],eval('_'+sp2)[p2])  in SAVED)
+            print("broken")
+            #print what you need here if there are bugs:-(
+            break
+    
+    fread.close()
+    fwrite.close()
+
+
