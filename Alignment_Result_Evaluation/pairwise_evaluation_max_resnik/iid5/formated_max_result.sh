601c69b3e0b042118a2fe20780ad07f287c42f94
wws <351483565@qq.com>
2019-05-31 14:48:26 -0700

    fixing bugs in foramted_max.sh

--- Alignment_Result_Evaluation/pairwise_evaluation_max_resnik/iid5/formated_max_result.sh
+++ Alignment_Result_Evaluation/pairwise_evaluation_max_resnik/iid5/formated_max_result.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rename .txt .align *.txt
+#rename .txt .align *.txt # only when not using converse.py
 sh max_result.sh > max_result_iid5.txt
 python3 format_max_result.py
 rm max_result_iid5.txt
